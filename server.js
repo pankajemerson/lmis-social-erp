5b73ad26631c3cf361bc4609a3af2a7a6253e3e4
pankajemerson <pankaj.wahane@emerson.com>
2015-01-17 21:13:49 +0530

    Comments..

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
+//Declare Dependencies
 var restify = require('restify');
 var mongojs = require('mongojs');
 var db = mongojs('mongodb://localhost:27017/deviceem', ['FreightDetails']);
 
-
-// Server
+// Create a Restify Server to serve Restful requests.
 var server = restify.createServer();
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
+//Start the hhtp server
 server.listen(3000, function () {
 	console.log("Server started @ 3000");
 });
-
 
 //Query like http://IP_ADDRESS:3000/freights?truckNumber=HR74-4967
 server.get("/freights", function (req, res, next) {	
