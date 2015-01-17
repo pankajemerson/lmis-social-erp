6051f813a6e4673df022b138afb17ce414c174ed
pankajemerson <pankaj.wahane@emerson.com>
2015-01-17 20:14:23 +0530

    Simple Usage comment

--- server.js
+++ server.js
@@ -13,9 +13,11 @@ server.listen(3000, function () {
 	console.log("Server started @ 3000");
 });
 
+
+//Query like http://IP_ADDRESS:3000/freights?truckNumber=HR74-4967
 server.get("/freights", function (req, res, next) {	
 	db.FreightDetails.find({truckNumber:req.params.truckNumber},function (err, symbols) {
                res.end(JSON.stringify(symbols));
-    });
+    	});
     return next();
 });
