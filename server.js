5cbe4fc075b64b08dbd2476ccc4bec5fcd878743
Pankaj Wahane <pankajemerson@users.noreply.github.com>
2015-01-18 19:35:35 +0530

    Post Added

--- server.js
+++ server.js
@@ -14,12 +14,24 @@ server.listen(3000, function () {
 	console.log("Server started @ 3000");
 });
 
-//Query like http://IP_ADDRESS:3000/freights?truckNumber=HR74-4967
 server.get("/devices", function (req, res, next) {	
 	
 		db.devicedb.find(function (err, devices) {
 	        	res.end(JSON.stringify(details));
     		});
 	
+    return next();
+});
+
+
+server.post('/device', function (req, res, next) {
+    var device = req.params;
+    db.products.save(device,
+        function (err, data) {
+            res.writeHead(200, {
+                'Content-Type': 'application/json; charset=utf-8'
+            });
+            res.end(JSON.stringify(data));
+        });
     return next();
 });
